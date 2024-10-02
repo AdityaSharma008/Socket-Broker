25c50503e0990790c2451f55754c178b790d9a41
Aditya Sharma <121496528+AdityaSharma008@users.noreply.github.com>
2024-10-03 00:20:32 +0530

    refactor

--- src/KafkaBroker.java
+++ src/KafkaBroker.java
@@ -1,9 +1,10 @@
 import java.net.*;
 
 public class KafkaBroker {
+    private static final int PORT = 9092;
     public static void main(String[] args) {
         try{
-            ServerSocket serverSocket = new ServerSocket(9092);
+            ServerSocket serverSocket = new ServerSocket(PORT);
             Socket client = serverSocket.accept();
         }catch (Exception e){
             e.printStackTrace();

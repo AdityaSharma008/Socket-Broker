a58c91fbea5adce1e689aa150ce86814193de97d
Aditya Sharma <121496528+AdityaSharma008@users.noreply.github.com>
2024-10-03 20:55:03 +0530

    Temp code to send correlation id to client

--- src/KafkaBroker.java
+++ src/KafkaBroker.java
@@ -1,14 +1,55 @@
+import java.io.*;
 import java.net.*;
 
 public class KafkaBroker {
     private static final int PORT = 9092;
+
     public static void main(String[] args) {
         try{
             ServerSocket serverSocket = new ServerSocket(PORT);
-            Socket client = serverSocket.accept();
-        }catch (Exception e){
+            while (true) {
+                Socket client = serverSocket.accept();
+                System.out.println("Client Connected");
+                handleClient(client);
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+    private static void handleClient(Socket client){
+        try {
+            DataInputStream in = new DataInputStream(client.getInputStream());
+            DataOutputStream out = new DataOutputStream(client.getOutputStream());
+
+            out.write(createMessage(7));
+
+            in.close();
+            out.close();
+            client.close();
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static byte[] createMessage(int id){
+        byte[] idBytes = intToByteArray(id);
+        byte[] lenBytes = intToByteArray(idBytes.length);
+
+        byte[] both = new byte[lenBytes.length + idBytes.length];
+        System.arraycopy(lenBytes, 0, both, 0, lenBytes.length);
+        System.arraycopy(idBytes, 0, both, lenBytes.length, idBytes.length);
+
+        return both;
+    }
+
+    private static byte[] intToByteArray(int n){
+        return new byte[] {
+                (byte)(n >> 24),
+                (byte)(n >> 16),
+                (byte)(n >> 8),
+                (byte)n
+        };
     }
 }
